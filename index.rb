f0c71885222f5662b1928c599fd7ad3d618891b1
jonnyps <jonny_spry@msn.com>
2017-05-10 22:16:19 +0100

    get letters from name

--- index.rb
+++ index.rb
@@ -1,4 +1,5 @@
 names = Array.new([]);
+alphPos = Array.new(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"])
 
 File.open("names.txt").each do |line|
 	# split the text file into individual names
@@ -12,21 +13,45 @@ File.open("names.txt").each do |line|
 	# remove quotation marks from each name
 	sorted.each { |n| 
 		n.gsub! /"/, ''
+	}
+	# get length of each name
+	sorted.each { |n| 
 		puts n.length
 	}
 	# display names
 	puts sorted
+
+	# get index of each name
 	sorted.each_with_index {
 		|val, index| puts "#{val} => #{index}" 
 	}
 
+	sorted.each { |n| 
+		thisName = n 
+		puts thisName
+		puts thisName.length
+			thisName.split("").each do |i|
+				puts i 
+			end
+	}
+
+	# get individual letters
+	# sorted.each { |n| 
+		# n.split("").each do |i|
+			# puts i
+		# end
+	# }
+	# sorted.each { |n|
+	# 	n.scan /\w/
+	# 	puts n
+	# }
+
 end
 
-# iterate through array
-# get length of each name
+# get each name from text file
+# push into array
+# sort alphabetically
+# remove quote marks
 # get position of name in array
-
-
-
-
-
+# get length of each name
+# 
